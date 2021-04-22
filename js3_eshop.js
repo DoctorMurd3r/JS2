0c48844e3a33fed1e9754fefac9005cd6fdb0008
DoctorMurd3r <75212227+DoctorMurd3r@users.noreply.github.com>
2021-04-22 12:51:28 +0500

    Create js3_eshop.js

---
+++ js3_eshop.js
@@ -0,0 +1,109 @@
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+function makeGETRequest(url, callback) {
+  let xhr;
+
+  if (window.XMLHttpRequest) {
+    xhr = new XMLHttpRequest();
+  } else if (window.ActiveXObject) { 
+    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      callback(xhr.responseText);
+    }
+  }
+
+  console.log('`${API_URL}${url}` :>> ', `${API_URL}${url}`);
+  xhr.open('GET', `${API_URL}${url}`, true);
+  xhr.send();
+};
+
+
+const makeGETRequest(url){
+    return new Promise(resolve,reject)=>{
+        let xhr;
+
+  if (window.XMLHttpRequest) {
+    xhr = new XMLHttpRequest();
+  } else if (window.ActiveXObject) { 
+    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+    }
+}
+
+makeGETRequest.then
+
+
+
+
+
+
+
+
+
+
+class GoodsItem {
+  constructor(product_name, price) {
+    this.product_name = product_name;
+    this.price = price;
+  }
+
+  render() {
+    return `<div class="goods-item"><h3>${this.product_name}</h3><p>${this.price}</p></div>`;
+  }
+}
+
+class GoodsList {
+  constructor() {
+    this.goods = [];
+  }
+
+  fetchGoods(cb) {
+    makeGETRequest(`/catalogData.json`, (goods) => {
+      this.goods = JSON.parse(goods);
+      cb();
+    })
+  }
+
+  render() {
+    let listHtml = '';
+    this.goods.forEach(good => {
+      const goodItem = new GoodsItem(good.product_name, good.price);
+      listHtml += goodItem.render();
+    });
+    document.querySelector('.goods-list').innerHTML = listHtml;
+  }
+
+  getTotalSum() {
+    return this.goods.forEach((item) => totalSum += item.price);
+  }
+}
+
+class GoodsCart extends GoodsList {
+  constructor() {
+    super();
+  }
+
+  clear() {}
+
+  buyAll() {}
+}
+
+class CartItem extends GoodsItem {
+  constructor() {
+    super();
+  }
+
+  delete() {}
+
+  addOne() {}
+
+  removeOne() {}
+}
+
+const list = new GoodsList();
+list.fetchGoods(() => {
+  list.render();
+});
